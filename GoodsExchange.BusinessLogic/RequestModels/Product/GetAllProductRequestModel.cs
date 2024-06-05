645f453d5054dc51d7112e9973020d1c1a259162
quoctrieu010903 <luongquoctrieu165@gmail.com>
2024-06-05 20:12:04 +0700

    update productviewmodel and createproductRequest

--- GoodsExchange.BusinessLogic/RequestModels/Product/GetAllProductRequestModel.cs
+++ GoodsExchange.BusinessLogic/RequestModels/Product/GetAllProductRequestModel.cs
@@ -14,6 +14,9 @@ namespace GoodsExchange.BusinessLogic.RequestModels.Product
         public DateTime? StartUploadDate { get; set; }
         public DateTime? EndUploadDate { get; set; }
         public DateTime? StartApprovedDate { get; set; }
+        public bool? IsApproved { get; set; }
+
+        public bool? IsActive { get; set; }
         public DateTime? EndApprovedDate { get; set; }
         public string? CategoryName { get; set; }
     }
